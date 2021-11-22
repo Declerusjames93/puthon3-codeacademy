4bb423fe9f338e6b2af190e0d1a8a2d657d8072e
James Declerus <james.declerus@esih.edu>
2021-11-22 16:09:24 -0500

    git commit

--- sal-shipping.py
+++ sal-shipping.py
@@ -1,4 +1,4 @@
-weight =  8.4
+weight =  4.8
 #Groung shipping
 if weight <= 2:
   cost = weight*1.5 + 20
@@ -9,12 +9,12 @@ elif weight > 6 and weight <= 10:
 else:
   cost = weight*4.75 + 20
 
+print("Groung shipping cost is: $ ", cost)
 #groung Shipping Premium
 ground_premium_cost = 125.00
 print("Ground shipping premium cost is: " + " $ " + str(ground_premium_cost))
 
 #Drone Shipping
-weight = 1.5
 if weight <= 2:
   cost = weight*4.5 + 0.00
 elif weight > 2 and weight <= 6:
@@ -23,4 +23,4 @@ elif weight > 6 and weight <= 10:
   cost = weight*12 +0.00
 else:
   cost = weight*14.25 + 0.00
-print(cost)
+print("Drone shipping cost is: $ ", cost)
